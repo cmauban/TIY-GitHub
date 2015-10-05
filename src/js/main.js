690ee028b6f23ae0861a63c859c362cd20f11bca
Camille Mauban <camille.mauban@gmail.com>
2015-10-05 17:44:46 -0400

    repo is always active

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
 //will give back a promise. //GETs content of one file
-jQuery.ajax('../api/github/users/octocat.json').then(function(data){
+// jQuery.ajax('../api/github/users/octocat.json').then(function(data){
+//
+//   console.log(arguments);
+//
+// });
 
-  console.log(arguments);
 
-});
+$('ul.tabLinks').each(function() {
+  // for each set of tabs..
+  // keeping track on which tab is active
+var $active, $content, $links = $('#repo').find('a');
+
+$active.addClass('active');
+
+// $('#repo').addClass('active');
+
+
+}); // END TAB LINKS
