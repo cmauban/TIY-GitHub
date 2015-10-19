cee4a2a6cc28f0faed8c801ab6d626d0b1dadbb7
Camille Mauban <camille.mauban@gmail.com>
2015-10-19 12:37:53 -0400

    using angular to get data

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-//will give back a promise. //GETs content of one file
+// will give back a promise. //GETs content of one file
 // jQuery.ajax('../api/github/users/octocat.json').then(function(data){
 //
 //   console.log(arguments);
@@ -38,31 +38,23 @@ $.getJSON('../api/github/users/cmauban.json')
 
 
 
-$.ajax ({
-  url: cmaubanAPI,
-  dataType: 'json',
-  success: function (data) {
-
-    $('.name').text(data.name);
-    $('.login').text(data.login);
-    $('li:eq(2)').text(data.email);
-    $('li:eq(4)').text(data.created_at);
-
-
-  }
-
-
-
-
-});
-
-
+// $.ajax ({
+//   url: cmaubanAPI,
+//   dataType: 'json',
+//   success: function (data) {
+//
+//     $('.name').text(data.name);
+//     $('.login').text(data.login);
+//     $('li:eq(2)').text(data.email);
+//     $('li:eq(4)').text(data.created_at);
+//
+//   }
+//
+// });
 
 // _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 // var complied = _.template('hello {{name}}!');
 //  compiled({ 'name': 'camille' });
-
-
 
 // function displayData (cmaubanAPI) {
 //   var template = $('#name').html();
@@ -75,33 +67,43 @@ $.ajax ({
 //   displayData(cmaubanAPI.name);
 // });
 
-
-
-
-
-
-
-
 // var compiled = _.template('<template><%- value %></template>');
 // compiled({ 'value': '<script>' });
 
 
 
+//TABS
 
-$('.tabs').each(function(){ //iterates through each ahref
-  $('.tabContent').each(function(){ //iterates through the corresponding content
-
-  $('a').on('click', function(event){
+  $('.tabs > a').on('click', function(event){
     event.preventDefault(); //stop browser to take action for clicked
-
-    $(this).addClass('active').siblings().removeClass('active'); //current href clicked is active
 
     var active = $(this).attr('href'); //targets active tab and grabs that href
 
-    $(active).addClass('active').siblings().removeClass('active'); //activates the corresponding content
+    $(this).add(active) // if use this, remove both each functions
+      .addClass('active')
+    .siblings()
+      .removeClass('active');
 
 
+    }).filter('[href="#panel-repo"]')
+      .trigger('click');
+// }); // END document.ready
+// }) (jQuery.noConflict())
 
+
+;(function(){ //IIFE
+  var app = angular.module('TIY-GitHub', [ ]);
+    // .controller('GenerticController', function($http, $scope){
+    app.run(function($http, $rootScope) {
+      $rootScope.name = 'The Octocat';
+      $rootScope.login = 'octocat';
+
+      $http.get('../api/github/users/cmauban.json')
+        .then(function(response){
+          $rootScope.user = response.data;
+
+          console.log(arguments);
+        });
     });
-  });
-});
+
+})(); // END IIFE
